4d50f6a40b3789b5401eba7595c52eed83735433
cbeserra <christopherbeserra@gmail.com>
2020-08-29 15:42:17 -0500

    first commit

--- selecting_elements/index.js
+++ selecting_elements/index.js
@@ -1,3 +1,2 @@
-function displayResult(){
-    document.getElementById<"display").innerHTML = "There can be only one!";
-}
+var hello = document.getElementById("header").innerHTML = "Hello from Texas!";
+console.log(header);

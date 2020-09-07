fdc0f4627e12cd827ea9cb8b819f46ee4bd502d6
cbeserra <christopherbeserra@gmail.com>
2020-09-06 21:06:33 -0500

    first commit

--- Week2/creating&Appending_elements/index.js
+++ Week2/creating&Appending_elements/index.js
@@ -2,13 +2,32 @@ var newH = document.createElement("H1")
 newH.textContent = "Welcome to my JS site"
 document.body.prepend(newH)
 
-
 var newP = document.createElement("P")
 newP.textContent = "All of this was created with Javascript"
 document.body.append(newP)
 
+var list = document.createElement('ol');
+list.setAttribute("id", "OL1")
+document.body.appendChild(list);
 
-var newLi = document.createElement("li")
-newLi.textContent = "poodle"
-var myLists = document.getElementById("lists")
-myLists.append(newLi)
+var newLiOne = document.createElement("li");
+document.getElementById("OL1")
+list.append(newLiOne);
+
+var newLiTwo = document.createElement("li");
+document.getElementById("OL1")
+list.append(newLiTwo);
+
+var newLiThree = document.createElement("li");
+document.getElementById("OL1")
+list.append(newLiThree);
+console.dir(list);
+
+newLiOne.textContent = "Football season is near!"
+list.append(newLiOne);
+
+newLiTwo.textContent = "Ready for some NFL!!"
+list.append(newLiTwo);
+
+newLiThree.textContent = "Go Niners - Revenge tour!!"
+list.append(newLiThree);

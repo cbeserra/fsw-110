e3c91227da1cde676057c4aac8d897a94eb7c38e
cbeserra <christopherbeserra@gmail.com>
2020-09-13 00:04:21 -0500

    first commit

---
+++ Week3/creating_a_lis/index.js
@@ -0,0 +1,27 @@
+
+for (var i = 0; i < 10; i++) {
+    var h1tag = document.createElement("H1");
+    h1tag.setAttribute("id", "h1Txt");
+    h1tag.textContent = "Hello World";
+    document.body.appendChild(h1tag);
+    h1tag.style.color = "red";
+    h1tag.style.border = "thick solid #0000ff";
+    h1tag.style.backgroundColor = "green";
+}
+
+
+
+const arr = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"];
+
+const cont = document.getElementById("container");
+const ul = document.createElement("ul");
+
+ul.setAttribute("id", "theList");
+for (i = 0; i <= arr.length - 1; i++) {
+    
+    const li = document.createElement("li");	
+    
+    li.innerHTML = arr[i];	                        
+    ul.appendChild(li);	
+}
+cont.appendChild(ul);		
